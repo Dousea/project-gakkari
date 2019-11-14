d2159b084df6ab4f7a60e48bcba4f235e088c141
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-14 15:52:44 +0700

    Changed TagsInput function to class; Fixed state `tags` wasn't updated on props changed

--- app/javascript/components/tags_input.jsx
+++ app/javascript/components/tags_input.jsx
@@ -1,42 +1,63 @@
 import React from 'react';
 
-function TagsInput(props) {
-	const [tags, setTags] = React.useState(props.tags);
-	const removeTags = indexToRemove => {
-    props.onRemoveTag(indexToRemove);
-		setTags([...tags.filter((_, index) => index !== indexToRemove)]);
-	};
-	const addTags = event => {
+class TagsInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: []
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // It will be probably error-prone
+    if (JSON.stringify(prevProps.tags) !== JSON.stringify(this.props.tags))
+      this.setState({ tags: this.props.tags });
+  }
+
+  handleRemoveTag(index) {
+    this.props.onRemoveTag(index);
+    this.state.tags.splice(index, 1);
+    this.setState({ tags: this.state.tags });
+  }
+
+  handleAddTag(event) {
     // props.handleKey will be included in the `value` so we need to remove it
     // by subbing the string
     let tag = event.target.value.substring(0, event.target.value.length-1);
 
 		if (tag !== "") {
-      setTags([...tags, tag]);
-      props.onAddTag(tags.length, tag);
+      this.state.tags.push(tag);
+      this.setState({ tags: this.state.tags });
+      this.props.onAddTag(this.state.tags.length-1, tag);
 			event.target.value = "";
 		}
-	};
-	return (
-		<div className="tags-input">
-			<ul id="tags">
-				{tags.map((tag, index) => (
-					<li key={index} className="tag">
-						<span className='tag-title'>{tag}</span>
-						<span className='tag-close-icon'
-							onClick={() => removeTags(index)}>
-							x
-						</span>
-					</li>
-				))}
-			</ul>
-			<input
-				type="text"
-				onKeyUp={event => event.key === props.handleKey ? addTags(event) : null}
-				placeholder={props.placeholder}
-			/>
-		</div>
-	);
-};
+  }
+
+  render() {
+    return (
+      <div className="tags-input">
+        <ul id="tags">
+          {this.state.tags.map((tag, index) => {
+            return (
+              <li key={index} className="tag">
+                <span className='tag-title'>{tag}</span>
+                <span className='tag-close-icon'
+                  onClick={() => this.handleRemoveTag(index)}>
+                  x
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          onKeyUp={event => event.key === this.props.handleKey ? this.handleAddTag(event) : null}
+          placeholder={this.props.placeholder}
+        />
+      </div>
+    );
+  }
+}
 
 export default TagsInput;
