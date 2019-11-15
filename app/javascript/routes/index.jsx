cf2a3bc1c28247c9a3d4ba97f7e36088a4b691ca
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-16 04:37:38 +0700

    Removed unnecessary `show` view for each book

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/home";
 import Books from "../components/books";
-import Book from "../components/book";
 import NewBook from "../components/new_book";
 import EditBook from "../components/edit_book";
 
@@ -11,7 +10,6 @@ export default (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/books" exact component={Books} />
-      <Route path="/books/:id" exact component={Book} />
       <Route path="/books/:id/edit" exact component={EditBook} />
       <Route path="/new_book" exact component={NewBook} />
     </Switch>
