324c09c36cd9117011ed53c78b711aa6f6ee324e
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-16 06:39:03 +0700

    Centers the jumbotron on `home` view

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default () => (
   <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
-    <div className="jumbotron">
+    <div className="jumbotron text-center">
       <h1 className="display-4">Perpustakaan Gakkari</h1>
       <p className="lead">Ayo cek persediaan buku dalam Perpustakaan Gakkari!</p>
       <hr className="my-4" />
