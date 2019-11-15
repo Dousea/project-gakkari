ecec1a788620c6cdc5b22853124cfb48bf18a548
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-16 03:46:04 +0700

    Modified `home` view style

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -2,22 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Perpustakaan Gakkari</h1>
-        <p className="lead">
-          Perpustakaan untuk membaca!
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/books"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Lihat Katalog
-        </Link>
-      </div>
+  <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+    <div className="jumbotron">
+      <h1 className="display-4">Perpustakaan Gakkari</h1>
+      <p className="lead">Ayo cek persediaan buku dalam Perpustakaan Gakkari!</p>
+      <hr className="my-4" />
+      <Link to="/books" className="btn btn-lg btn-primary" role="button">Masuk</Link>
     </div>
   </div>
 );
