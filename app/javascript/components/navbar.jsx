39724eea94d8aa9ba95d22cb1f17da0681fdeea3
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-16 06:59:45 +0700

    Now navbar responds on Router location path

--- app/javascript/components/navbar.jsx
+++ app/javascript/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { matchPath, Link } from "react-router-dom";
 import $ from "jquery";
 
 import "../stylesheets/navbar.scss";
@@ -21,25 +22,19 @@ class Navbar extends React.Component {
   }
 
   render() {
+    const isCatalogActive = !!matchPath(this.props.location.pathname, "/books");
+    const srOnlyCurrent = (<span className="sr-only">(current)</span>);
+
     return (
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-        <span className="navbar-brand mr-auto mr-lg-0">Project Gakkari</span>
+        <span className="navbar-brand mr-auto mr-lg-0">Perpustakaan Gakkari</span>
         <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="navbar-collapse offcanvas-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Notifications</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Profile</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Switch account</a>
+            <li className={`nav-item ${isCatalogActive && "active"}`}>
+              <Link to="/books" className="nav-link">Katalog {isCatalogActive && srOnlyCurrent}</Link>
             </li>
           </ul>
         </div>
