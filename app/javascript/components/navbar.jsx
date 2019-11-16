c4bb49c455387053dab9f6a09b93c96e04f7177f
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-16 07:22:25 +0700

    Automates the process of active item on navbar

--- app/javascript/components/navbar.jsx
+++ app/javascript/components/navbar.jsx
@@ -22,20 +22,33 @@ class Navbar extends React.Component {
   }
 
   render() {
-    const isCatalogActive = !!matchPath(this.props.location.pathname, "/books");
+    const navItems = [
+      /*
+      { name: "Katalog", path: "/books" },
+      { name: "Buku Baru", path: "/new_book" }
+      */
+    ]
+
+    const isActive = navItems.map(item => !!matchPath(this.props.location.pathname, item.path));
     const srOnlyCurrent = (<span className="sr-only">(current)</span>);
 
     return (
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <span className="navbar-brand mr-auto mr-lg-0">Perpustakaan Gakkari</span>
-        <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        {
+          // It should be only testing
+          navItems.length > 0 &&
+          <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        }
         <div className="navbar-collapse offcanvas-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className={`nav-item ${isCatalogActive && "active"}`}>
-              <Link to="/books" className="nav-link">Katalog {isCatalogActive && srOnlyCurrent}</Link>
-            </li>
+            {navItems.map((item, index) => (
+              <li className={`nav-item ${isActive[index] && "active"}`}>
+                <Link to={item.path} className="nav-link">{item.name} {isActive[index] && srOnlyCurrent}</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
