d76145a8090398c5cc853b380a3ee3b11c89c687
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-17 14:04:58 +0700

    Added published_at, authors, subjects validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,25 @@
 class Book < ApplicationRecord
-  validates :title, presence: true
   belongs_to :publisher
   has_and_belongs_to_many :authors
   has_and_belongs_to_many :subjects
+  validates :title, presence: true
+  validate :published_at_is_valid_date
+  validate :must_have_authors
+  validate :must_have_subjects
   accepts_nested_attributes_for :authors, allow_destroy: true
   accepts_nested_attributes_for :subjects, allow_destroy: true
+
+  private
+
+  def published_at_is_valid_date
+    Date.parse(published_at.to_s) rescue errors.add(:published_at, 'must be a valid date')
+  end
+
+  def must_have_authors
+    errors.add(:authors, 'must have at least one author') if authors.all?(&:marked_for_destruction?)
+  end
+
+  def must_have_subjects
+    errors.add(:subjects, 'must have at least one subject') if subjects.all?(&:marked_for_destruction?)
+  end
 end
