fad8c4489f6cb179b0585e8b539274dcc6b9bfa9
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-21 21:48:43 +0700

    Removed unnecessary navigation bar

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "../components/navbar";
 import Home from "../components/home";
 import Books from "../components/books";
-
-const NavRoute = ({path, exact, component: Component}) => (
-  <Route path={path} exact={exact} render={(props) => (
-    <div>
-      <Navbar {...props}/>
-      <Component {...props}/>
-    </div>
-  )}/>
-)
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <NavRoute path="/books" exact component={Books} />
+      <Route path="/books" exact component={Books} />
     </Switch>
   </Router>
 );
