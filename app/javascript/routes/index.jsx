ce479c7384520df65d93aa8cbd691061628bd598
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-23 01:44:17 +0700

    Added redirect any path to home view

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../components/home";
 import Books from "../components/books";
 
@@ -8,6 +8,7 @@ export default (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/books" exact component={Books} />
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
