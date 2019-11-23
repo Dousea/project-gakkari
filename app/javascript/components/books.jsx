5d571803defe249bb8c9cd0b0a66742718ec1e67
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2019-11-23 16:36:36 +0700

    Added layering for toasts

--- app/javascript/components/books.jsx
+++ app/javascript/components/books.jsx
@@ -131,7 +131,15 @@ class Books extends React.Component {
       );
     
     return (
-      <div aria-live="polite" aria-atomic="true">
+      <div id="content" aria-live="polite" aria-atomic="true" className="position-relative">
+        <div id="alert-toast" className="toast" role="alert" data-delay="3000" aria-live="assertive" aria-atomic="true">
+          <div className="toast-header">
+            <strong id="alert-toast-text" className="mr-auto" />
+            <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
         <section className="jumbotron jumbotron-fluid bg-dark text-center">
           <div className="container">
             <h1 className="jumbotron-heading text-white">Katalog</h1>
@@ -158,14 +166,6 @@ class Books extends React.Component {
             }
             {pagination}
             <BookForm />
-            <div id="alert-toast" className="toast" role="alert" data-delay="3000" aria-live="assertive" aria-atomic="true">
-              <div className="toast-header">
-                <strong id="alert-toast-text" className="mr-auto" />
-                <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
