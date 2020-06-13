76bef0319c799cea3a7b56c91841f652b86e45c8
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-13 23:37:15 +0700

    Fixed unclickable user button on topbar

--- app/javascript/components/dashboard.jsx
+++ app/javascript/components/dashboard.jsx
@@ -52,9 +52,9 @@ class Dashboard extends React.Component {
 
       return (
         <>
-          <Topbar member={this.state.memberInfo} handlePage={this.handlePage} />
+          <Topbar member={this.state.memberInfo} currentPage={this.state.page} handlePage={this.handlePage} />
           <div id='content' aria-live='polite' aria-atomic='true' className='position-relative'>
-            <div id='alert-toast' className='toast' role='alert' data-delay='3000' aria-live='assertive' aria-atomic='true'>
+            <div id='alert-toast' className='toast hide' role='alert' data-delay='3000' aria-live='assertive' aria-atomic='true'>
               <div className='toast-header'>
                 <strong id='alert-toast-text' className='mr-auto' />
                 <button type='button' className='ml-2 mb-1 close' data-dismiss='toast' aria-label='Close'>
